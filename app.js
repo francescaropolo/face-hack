94e26feedeebe0db77fd536faeb51084edb736db
Nixie Skywalker <nyxscream@gmail.com>
2018-07-30 11:39:23 +0200

    add signup GET route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
 
 mongoose.connect('mongodb://localhost/facehack');
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
