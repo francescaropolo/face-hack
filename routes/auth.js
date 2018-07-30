69a9d105e7b113805a74d511d49e911a54e26bcb
Nixie Skywalker <nyxscream@gmail.com>
2018-07-30 11:44:16 +0200

    add login GET route

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,16 @@ router.get('/signup', (req, res, next) => {
     res.render('auth/signup');
 });
 
+router.post('/signup', (req, res, next) => {
+
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('auth/login');
+});
+
+router.post('/login', (req, res, next) => {
+
+});
+
 module.exports = router;
