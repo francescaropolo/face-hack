4f5d1efa8be6e3d4f805c3ae4d59ef0b0da60463
Nixie Skywalker <nyxscream@gmail.com>
2018-07-30 11:56:41 +0200

    add login POST route

--- routes/auth.js
+++ routes/auth.js
@@ -10,12 +10,12 @@ router.get('/signup', (req, res, next) => {
 });
 
 router.post('/signup', (req, res, next) => {
-    const {username, password} = req.body;
-    if (!username || !password) {
+    const {email, password} = req.body;
+    if (!email || !password) {
         req.flash('info', 'Los campos son obligatorios');
         return res.redirect('signup');
     }
-    User.findOne({username})
+    User.findOne({email})
         .then(user => {
             if (user) {
                 req.flash('info', 'Usuario ya existente');
@@ -23,7 +23,7 @@ router.post('/signup', (req, res, next) => {
             } else {
                 const salt = bcrypt.genSaltSync(saltRounds);
                 const hashedPassword = bcrypt.hashSync(password, salt);
-                const newUser = new User({username, password: hashedPassword});
+                const newUser = new User({email, password: hashedPassword});
                 newUser.save()
                     .then(user => {
                         req.session.currentUser = user;
@@ -44,7 +44,28 @@ router.get('/login', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
-
+    const {email, password} = req.body;
+    if (!email || !password) {
+        req.flash('info', 'Los campos son obligatorios');
+        return res.redirect('/login');
+    }
+    User.findOne({email})
+        .then(user => {
+            if (!user) {
+                req.flash('info', 'No existe el usuario');
+                return res.redirect('/login');
+            }
+            if (bcrypt.compareSync(password, user.password)) {
+                req.session.currentUser = user;
+                res.redirect('/');
+            } else {
+                req.flash('info', 'Contraseña incorrecta');
+                res.redirect('/login');
+            }
+        })
+        .catch(error => {
+            next(error);
+        });
 });
 
 module.exports = router;
