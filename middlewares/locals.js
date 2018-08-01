cbf51db08105a7040e3a4a56661dd42c525d8c80
Nixie Skywalker <nyxscream@gmail.com>
2018-08-01 16:55:51 +0200

    style buttons and navbar

--- middlewares/locals.js
+++ middlewares/locals.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 
 const localCurrentUser = (req, res, next) => {
-  app.locals.currentUser = req.session.currentUser;
-  next();
-}
+    app.locals.currentUser = req.session.currentUser;
+    next();
+};
 
 module.exports = {
-  localCurrentUser
-}
+    localCurrentUser
+};
