dcbc805e2d250d729ce1804f2c2c43309e97a1eb
Nixie Skywalker <nyxscream@gmail.com>
2018-08-02 19:33:54 +0200

    fix eslint requirements

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,21 +5,21 @@ const router = express.Router();
 const ObjectId = require('mongodb').ObjectID;
 const Job = require('../models/job');
 const moment = require('moment');
-moment().format('Do MMMM YYYY')
+moment().format('Do MMMM YYYY');
 
 // GET jobs listing only logged user jobs
 router.get('/', (req, res, next) => {
     const data = {
         sessionFlash: res.locals.sessionFlash
     };
-    const oid = req.session.currentUser._id;    
+    const oid = req.session.currentUser._id;
     Job.find({'owner': ObjectId(oid)})
         .populate('owner')
-        .then((jobs) => {     
+        .then((jobs) => {
             const jid = jobs._id;
             const jobTime1 = ObjectId(jid).getTimestamp(); // Getting date of creation
-            const jobTime2 = jobTime1.toString(); // Parsing raw mongo date to string 
-            const jobTime = moment(jobTime2).format('Do MMMM YYYY'); // Parsing using moment.js to new date format            
+            const jobTime2 = jobTime1.toString(); // Parsing raw mongo date to string
+            const jobTime = moment(jobTime2).format('Do MMMM YYYY'); // Parsing using moment.js to new date format
             res.render('jobs/my-jobs', { jobs, data, jobTime });
         })
         .catch(error => {
